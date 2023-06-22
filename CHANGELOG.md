6a5ef83e71a98b1dfdcfcd8706e1f03d57cdacf1
Quin Henshaw <qhenshaw@gmail.com>
2023-06-22 13:28:49 -0700

    - added debug system + sample - updated package info

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,5 @@
 # Changelog
 
-[1.0.0] - 2022-10-23
-- This is the first release of [Package Name]
+[1.0.0] - 2023-06-22
+- This is the first release of Debug Menu
+- Includes DebugMenuSystem script and sample prefabs for drop in functionality
---
+++ Runtime/DebugMenuSystem.cs
@@ -0,0 +1,147 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using System.Linq;
+using System.Reflection;
+using TMPro;
+using UnityEngine.EventSystems;
+
+namespace DebugMenu
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class DebugCommandAttribute : Attribute
+    {
+        public string Name { get; private set; }
+
+        public DebugCommandAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+
+    [DefaultExecutionOrder(-100)]
+    public class DebugMenuSystem : MonoBehaviour
+    {
+        private class DebugCommandPair
+        {
+            public object Target;
+            public MethodInfo Method;
+
+            public DebugCommandPair(object target, MethodInfo method)
+            {
+                Target = target;
+                Method = method;
+            }
+        }
+
+        [SerializeField] private KeyCode _toggleKey = KeyCode.Tilde;
+        [SerializeField] private CursorLockMode _closedCursorMode = CursorLockMode.Locked;
+        [SerializeField] private bool _closedCursorVisible = false;
+        [SerializeField] private Transform _panel;
+        [SerializeField] private Transform _buttonLayout;
+        [SerializeField] private Button _buttonPrefab;
+
+        public static DebugMenuSystem Instance { get; private set; }
+
+        private Dictionary<string, List<DebugCommandPair>> _commands;
+
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                Debug.LogWarning("Duplicate DebugMenuSystem found, destroying!");
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            _commands = new Dictionary<string, List<DebugCommandPair>>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(_toggleKey))
+            {
+                if (!_panel.gameObject.activeInHierarchy) Open();
+                else Close();
+            }
+        }
+
+        private void Open()
+        {
+            _panel.gameObject.SetActive(true);
+
+            foreach (var command in _commands)
+            {
+                Button button = Instantiate(_buttonPrefab);
+                button.GetComponentInChildren<TextMeshProUGUI>().text = command.Key;
+                button.transform.SetParent(_buttonLayout);
+
+                foreach (var commandPair in command.Value)
+                {
+                    button.onClick.AddListener(() => commandPair.Method.Invoke(commandPair.Target, null));
+                }
+            }
+
+            if (_buttonLayout.childCount >= 1) EventSystem.current.SetSelectedGameObject(_buttonLayout.GetChild(0).gameObject);
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        private void Close()
+        {
+            for (int i = 0; i < _buttonLayout.childCount; i++)
+            {
+                Destroy(_buttonLayout.GetChild(i).gameObject);
+            }
+
+            _panel.gameObject.SetActive(false);
+
+            Cursor.lockState = _closedCursorMode;
+            Cursor.visible = _closedCursorVisible;
+        }
+
+        public void RegisterObject(object component)
+        {
+            var methods = component.GetType().
+                            GetMethods(BindingFlags.Public | BindingFlags.Instance).
+                            Where(x => x.GetCustomAttributes(typeof(DebugCommandAttribute), true).Any());
+
+            foreach (var method in methods)
+            {
+                DebugCommandAttribute att = (DebugCommandAttribute)method.GetCustomAttributes(typeof(DebugCommandAttribute), true).FirstOrDefault();
+                if (!_commands.ContainsKey(att.Name)) _commands.Add(att.Name, new List<DebugCommandPair>());
+                _commands[att.Name].Add(new DebugCommandPair(component, method));
+            }
+        }
+
+        public void DeregisterObject(object component)
+        {
+            var methods = component.GetType().
+                            GetMethods(BindingFlags.Public | BindingFlags.Instance).
+                            Where(x => x.GetCustomAttributes(typeof(DebugCommandAttribute), true).Any());
+
+            foreach (var method in methods)
+            {
+                DebugCommandAttribute att = (DebugCommandAttribute)method.GetCustomAttributes(typeof(DebugCommandAttribute), true).FirstOrDefault();
+                if (!_commands.ContainsKey(att.Name)) continue;
+                List<DebugCommandPair> pairs = _commands[att.Name];
+                DebugCommandPair match = null;
+                foreach (var pair in pairs)
+                {
+                    if (pair.Target == component && pair.Method == method)
+                    {
+                        match = pair;
+                        break;
+                    }
+                }
+                if (match != null) pairs.Remove(match);
+            }
+
+            _commands = _commands.Where(x => x.Value.Count() > 0).ToDictionary(x => x.Key, x => x.Value);
+        }
+    }
+}
